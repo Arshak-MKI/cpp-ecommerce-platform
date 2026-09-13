ad514347b65f5c511c246a625c745dbc98b29436
Arshak Eranosyan <arshak.eranosyan07@gmail.com>
2026-09-13 16:29:45 +0400

    Add database abstraction

--- main.cpp
+++ main.cpp
@@ -1,7 +1,33 @@
 #include <iostream>
 
+#include "database/Database.h"
+
 int main() {
-    std::cout << "C++ E-Commerce Platform" << std::endl;
+    Database database("database/shop.db");
+
+    if (!database.open()) {
+        return 1;
+    }
+
+    std::cout << "Database opened successfully!" << std::endl;
+
+    const std::string create_products_table = R"(
+        CREATE TABLE IF NOT EXISTS products (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            name TEXT NOT NULL UNIQUE,
+            price REAL NOT NULL,
+            description TEXT,
+            category TEXT,
+            image_path TEXT,
+            is_available INTEGER NOT NULL DEFAULT 1
+        );
+    )";
+
+    if (!database.execute(create_products_table)) {
+        return 1;
+    }
+
+    std::cout << "Products table created successfully!" << std::endl;
 
     return 0;
 }
